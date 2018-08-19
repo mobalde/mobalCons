8aeab2fa5eef1b0aebb08afdae7bf8a050e44680
unknown <mamoudous2005@yahoo.fr>
2018-08-20 00:19:19 +0200

    ws bilan compte

--- mobal.const/src/main/java/mobalDev/resources/vendu/VenduController.java
+++ mobal.const/src/main/java/mobalDev/resources/vendu/VenduController.java
@@ -1,13 +1,19 @@
 package mobalDev.resources.vendu;
 
+import java.util.List;
+
 import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import mobalDev.logic.vendu.GestionVente;
+import mobalDev.logic.vendu.dto.VenduDto;
 
 @CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
 @RestController
@@ -20,5 +26,9 @@ public class VenduController {
 	@Inject
 	private GestionVente gestionVente;
 	
-	
+	@RequestMapping(path = "/add", method = RequestMethod.POST)
+	public boolean saveVente(HttpSession session, @RequestBody List<VenduDto> listVente){
+		System.out.println("____ check");
+		return true;
+	}
 }
